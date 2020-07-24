75bb9766fc21fcabeb7ec567af6852c05dbc7192
BatteredBunny <ayes2022@protonmail.com>
2020-07-25 01:35:40 +0300

    Update nameMC.py

--- nameMC.py
+++ nameMC.py
@@ -1,37 +1,70 @@
+import json, requests, time, getpass
 from selenium import webdriver
-import json, requests, time
 
-# Browser used by the bot
-driver = webdriver.Firefox()
-
-# Replace this with some other friends list if you want!
-apiLink = "https://api.namemc.com/profile/b4769eac-1010-405a-b668-04a07b2f20f3/friends"
 waitTime = 60
-x = 0
 
+#gets the username and password so it can login to account
+username = input("Enter your username: ")
+password = getpass.getpass("Enter your password: ")
+print(" ")
+
+#accepts friend lists and gives default value
+print("Type nothing for default option.")
+apiLink = input("Insert friends list api link: ")
+if apiLink == "":
+	apiLink = "https://api.namemc.com/profile/b4769eac-1010-405a-b668-04a07b2f20f3/friends"
+
+# loads friend list as python dictionary
 friendsList = requests.get(apiLink)
 friendsNames = json.loads(friendsList.text)
 
-# You have to login or else it doesn't work
+
+# Opens browser and login page
+driver = webdriver.Firefox()
 driver.get("https://namemc.com/login")
-t = input("Press enter after you have logged in. ")
-print(" ")
+time.sleep(1)
+
+try:
+	# password, username and submit fields for the bot to use them
+	emailField = driver.find_element_by_id("email")
+	passwordField = driver.find_element_by_id("password")
+	submitButton = driver.find_element_by_xpath("//button[@class='btn btn-primary']")
+
+	#send the inputs and clicks button
+	passwordField.send_keys(password)
+	emailField.send_keys(username)
+	submitButton.click()
+except:
+	print(" ")
+	t = input("Please complete the captcha then press enter.")
+
+	# password, username and submit fields for the bot to use them
+	emailField = driver.find_element_by_id("email")
+	passwordField = driver.find_element_by_id("password")
+	submitButton = driver.find_element_by_xpath("//button[@class='btn btn-primary']")
+
+	#send the inputs and clicks button
+	passwordField.send_keys(password)
+	emailField.send_keys(username)
+	submitButton.click()
 
 for i in friendsNames:
-	while x <= waitTime:
-		time.sleep(1)
-		timeRemaining = waitTime - x
-		print("Wait: " + str(timeRemaining) + " Seconds")
-		x += 1
-	x = 0
 
 	driver.get("https://namemc.com/profile/" + i["name"])
+
+	print(" ")
+	print("Username: " + i["name"] + " UUID: " + i["uuid"])
+
 	try:
+
 		button = driver.find_element_by_id('add-friend-button')
 		button.click()
+
 	except:
-		print(" ")
-		print("I can't find the button, skipping it!")
-		
-	print("Username: " + i["name"])
-	print(" ")
+		print("I already sent friend request to them. (Or im not logged in)")
+		print("Waiting " + str(waitTime) + " Seconds")
+
+	else:
+		print("Request sent, waiting " + str(waitTime) + " Seconds")
+
+	time.sleep(waitTime)
